726c25e0c67a3f03cd0106f648d71017fd9e2593
Sarilus-Blaise <sarilusblondywadley@gmail.com>
2023-03-30 20:09:46 -0400

    get contacts list from API and synchronize it with the root route

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
-import Root from './routes/root';
+import Root, { loader as rootLoader } from './routes/root';
 import ErrorPage from './error-page';
 import Contact from './routes/contact';
 
@@ -12,6 +12,7 @@ const router = createBrowserRouter([
 		path: '/',
 		element: <Root />,
 		errorElement: <ErrorPage />,
+		loader: rootLoader,
 		children: [
 			{
 				path: 'contacts/:contactId',
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,5 +1,12 @@
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet, Link, useLoaderData } from 'react-router-dom';
+import { getContacts } from '../contact';
+//get contacts from the the getContact API , contact will stay in sync with the root route
+export async function loader() {
+	const contacts = await getContacts();
+	return { contacts };
+}
 export default function Root() {
+	const { contacts } = useLoaderData();
 	return (
 		<>
 			<div id='sidebar'>
@@ -21,14 +28,28 @@ export default function Root() {
 					</form>
 				</div>
 				<nav>
-					<ul>
-						<li>
-							<Link to={`contacts/1`}>Your Name</Link>
-						</li>
-						<li>
-							<Link to={`contacts/2`}>Your Friend</Link>
-						</li>
-					</ul>
+					{contacts.length ? (
+						<ul>
+							{contacts.map((contact) => (
+								<li key={contact.id}>
+									<Link to={`contacts/${contact.id}`}>
+										{contact.first || contact.last ? (
+											<>
+												{contact.first} {contact.last}
+											</>
+										) : (
+											<i>No Name</i>
+										)}{' '}
+										{contact.favorite && <span>★</span>}
+									</Link>
+								</li>
+							))}
+						</ul>
+					) : (
+						<p>
+							<i>No contacts</i>
+						</p>
+					)}
 				</nav>
 			</div>
 			<div id='detail'>
