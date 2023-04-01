f14d26c79a5455ccb5e210baf405d60958eb7bb0
Sarilus-Blaise <sarilusblondywadley@gmail.com>
2023-04-01 09:41:28 -0400

    highlighted active link in the sidebar

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,11 @@
-import { Outlet, Link, useLoaderData, Form, redirect } from 'react-router-dom';
+import {
+	Outlet,
+	Link,
+	useLoaderData,
+	Form,
+	redirect,
+	NavLink,
+} from 'react-router-dom';
 import { getContacts, createContact } from '../contact';
 //get contacts from the the getContact API , contact will stay in sync with the root route
 export async function loader() {
@@ -38,16 +45,23 @@ export default function Root() {
 						<ul>
 							{contacts.map((contact) => (
 								<li key={contact.id}>
-									<Link to={`contacts/${contact.id}`}>
-										{contact.first || contact.last ? (
-											<>
-												{contact.first} {contact.last}
-											</>
-										) : (
-											<i>No Name</i>
-										)}{' '}
-										{contact.favorite && <span>★</span>}
-									</Link>
+									<NavLink
+										to={`contacts/${contact.id}`}
+										className={({ isActive, isPending }) =>
+											isActive ? 'active' : isPending ? 'pending' : ''
+										}
+									>
+										<Link to={`contacts/${contact.id}`}>
+											{contact.first || contact.last ? (
+												<>
+													{contact.first} {contact.last}
+												</>
+											) : (
+												<i>No Name</i>
+											)}{' '}
+											{contact.favorite && <span>★</span>}
+										</Link>
+									</NavLink>
 								</li>
 							))}
 						</ul>
