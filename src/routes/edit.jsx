5e0a039b113f71c028cc1907cfb23f17737f3c58
Sarilus-Blaise <sarilusblondywadley@gmail.com>
2023-04-03 18:35:30 -0400

    added cancel bahavior

--- src/routes/edit.jsx
+++ src/routes/edit.jsx
@@ -1,4 +1,4 @@
-import { Form, useLoaderData, redirect } from 'react-router-dom';
+import { Form, useLoaderData, redirect, useNavigate } from 'react-router-dom';
 import { getContact, updateContact } from '../contact';
 
 export async function loader({ params }) {
@@ -15,6 +15,7 @@ export async function action({ request, params }) {
 
 export default function EditContact() {
 	const { contact } = useLoaderData();
+	const navigate = useNavigate();
 
 	return (
 		<Form method='post' id='contact-form'>
@@ -60,7 +61,9 @@ export default function EditContact() {
 			</label>
 			<p>
 				<button type='submit'>Save</button>
-				<button type='button'>Cancel</button>
+				<button type='button' onClick={() => navigate(-1)}>
+					Cancel
+				</button>
 			</p>
 		</Form>
 	);
