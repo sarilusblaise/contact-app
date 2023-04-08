9c87854cc45c9b9be14150aa4a85370ef049169e
Sarilus-Blaise <sarilusblondywadley@gmail.com>
2023-04-07 21:22:57 -0400

    added custom error in contact page

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -4,6 +4,12 @@ import { getContact, updateContact } from '../contact';
 
 export async function loader({ params }) {
 	const contact = await getContact(params.contactId);
+	if (!contact) {
+		throw new Response('', {
+			status: 404,
+			statusText: 'Not Found',
+		});
+	}
 	return { contact };
 }
 
