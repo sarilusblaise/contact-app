d1200e4d6362cea7e213add6ae225a0671aed61e
Sarilus-Blaise <sarilusblondywadley@gmail.com>
2023-04-07 21:02:59 -0400

    made the favorite responsive

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -70,6 +70,9 @@ function Favorite({ contact }) {
 	// yes, this is a `let` for later
 	let favorite = contact.favorite;
 	const fetcher = useFetcher();
+	if (fetcher.formData) {
+		favorite = fetcher.formData.get('favorite') === 'true';
+	}
 	return (
 		<fetcher.Form method='post'>
 			<button
