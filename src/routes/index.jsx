e30eab55266fb291bda1c61bc3b99edfd77b1cd0
Sarilus-Blaise <sarilusblondywadley@gmail.com>
2023-04-09 10:47:03 -0400

    deleted logo in the index page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,6 @@
 export default function Index() {
 	return (
-		<p id='zero-state'>
+		<p>
 			Contact app is user friendly app that ley you manage your contacts in a
 			very well organized way
 		</p>
